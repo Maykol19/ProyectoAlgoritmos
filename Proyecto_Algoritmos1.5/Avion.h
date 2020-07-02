0173dacd29f3d4a550c567ef835aacda61e79967
Maykol19 <maykolmesen@gmail.com>
2020-07-01 18:13:07 -0600

    Logica de historial faltante y cambios generales en interfaz y clases

--- Proyecto_Algoritmos1.5/Avion.h
+++ Proyecto_Algoritmos1.5/Avion.h
@@ -8,22 +8,24 @@
 using namespace std;
 
 class Avion {
-    
     //atributo
     string nombre;
     int x;
     int y;
+    int horas;
 
 public:
     Avion(string nombre, int x, int y);
-    Avion(string nombre);
+    Avion(string nombre, int horas);
     void SetNombre(string nombre);
     string GetNombre() const;
     void SetX(int x);
     int GetX() const;
     void SetY(int y);
     int GetY() const;
+    int GetHoras() const;
     string toString();
+    string toStringH();
 
 };
 
